041cbac24b63ed61b55e914abf9c2450c8386270
Jung JaeWoong <jjw_0609@naver.com>
2024-06-28 22:46:12 +0900

    스크롤 높이 셋팅

---
+++ js/main.js
@@ -0,0 +1,52 @@
+(() => {
+    const sceneInfo = [
+        {
+            // 0
+            type: 'sticky',
+            heightNum: 5,   // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-0')
+            }
+        },
+        {
+            // 1
+            type: 'normal',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1')
+            }
+        },
+        {
+            // 2
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2')
+            }
+        },
+        {
+            // 3
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3')
+            }
+        }
+    ];
+
+    function setLayout() {
+        // 각 스크롤 섹션의 높이 세팅
+        for(let i=0 ; i<sceneInfo.length ; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+    }
+
+    window.addEventListener('resize', setLayout);
+
+    setLayout();
+})();
