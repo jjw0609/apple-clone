a527df0945464fe9bcafdf7ab6e9c90b0f79377d
Jung JaeWoong <jjw_0609@naver.com>
2024-07-02 21:22:39 +0900

    현재 활성시킬 씬 결정하기

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    let yOffset = 0;            // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0;   // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0;       // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -46,7 +51,32 @@
         }
     }
 
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+
+        for(let i=0 ; i<currentScene ; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if(yOffset < prevScrollHeight) {
+            if(currentScene === 0) return;
+            currentScene--;
+        }
+
+        console.log(currentScene);
+    }
+
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    })
 
     setLayout();
+
 })();
