14a79cd48e8dfb2b86d9646533c365b424a213cf
Jung JaeWoong <jjw_0609@naver.com>
2024-07-20 21:41:57 +0900

    스크롤 애니메이션3

--- js/main.js
+++ js/main.js
@@ -72,7 +72,14 @@
     }
 
     function calcValues(values, currentYOffset) {
+        let rv;
 
+        // 현재 씬(스크롤 section)에서 스크롤된 범위를 비율로 구하기
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+        rv = scrollRatio * (values[1] - values[0]) + values[0];
+
+        return rv;
     }
 
     function palyAnimation() {
@@ -83,10 +90,8 @@
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
-                let messageA_opacity_0 = values.messageA_opacity[0];
-                let messageA_opacity_1 = values.messageA_opacity[1];
-
-                calcValues(values, currentYOffset);
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+                objs.messageA.style.opacity = messageA_opacity_in;
 
                 break;
             case 1:
