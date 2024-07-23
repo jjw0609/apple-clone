2390f84533e6020639a415e4818af53fc12b6987
Jung JaeWoong <jjw_0609@naver.com>
2024-07-23 22:23:03 +0900

    특정 타이밍 스크롤 애니메이션

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d')
             },
             values: {
-                messageA_opacity: [0, 1]
+                messageA_opacity: [0, 1, {start: 0.1, end: 0.2}],
+                messageB_opacity: [0, 1, {start: 0.3, end: 0.4}]
             }
         },
         {
@@ -76,9 +77,28 @@
         let rv;
 
         // 현재 씬(스크롤 section)에서 스크롤된 범위를 비율로 구하기
-        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
 
-        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        if(values.length === 3) {
+            // start ~ end 사이에 애니메이션 실행
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart;
+
+            if(currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+
+            } else if(currentYOffset < partScrollStart) {
+                rv = values[0];
+
+            } else if(currentYOffset > partScrollEnd) {
+                rv = values[1];
+
+            }
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
 
         return rv;
     }
