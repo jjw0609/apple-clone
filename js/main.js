77bbc2c23ff3ff537dae5e1015b94218cd1c5263
wjdwodnd93 <jjw_0609@naver.com>
2024-07-27 22:47:14 +0900

    특정 타이밍 스크롤 애니메이션

--- js/main.js
+++ js/main.js
@@ -19,8 +19,12 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d')
             },
             values: {
-                messageA_opacity: [0, 1, {start: 0.1, end: 0.2}],
-                messageB_opacity: [0, 1, {start: 0.3, end: 0.4}]
+                messageA_opacity_in: [0, 1, {start: 0.1, end: 0.2}],
+                // messageB_opacity_in: [0, 1, {start: 0.3, end: 0.4}],
+                messageA_translateY_in: [20, 0, {start: 0.1, end: 0.2}],
+
+                messageA_opacity_out: [1, 0, {start: 0.25, end: 0.3}],
+                messageA_translateY_out: [0, -20, {start: 0.25, end: 0.3}],
             }
         },
         {
@@ -107,12 +111,26 @@
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
 
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
-                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
-                objs.messageA.style.opacity = messageA_opacity_in;
+                const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
+                const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
+                const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
+                const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
+
+                if(scrollRatio <= 0.22) {
+                    // in
+                    objs.messageA.style.opacity = messageA_opacity_in;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+                } else {
+                    // out
+                    objs.messageA.style.opacity = messageA_opacity_out;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+                }
 
                 break;
             case 1:
