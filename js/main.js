01b7f11637fb3a3531678d38aa4b2e890221fb90
wjdwodnd93 <jjw_0609@naver.com>
2024-08-21 21:21:34 +0900

    블렌딩 캔버스 크기 계산

--- js/main.js
+++ js/main.js
@@ -101,7 +101,14 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3'),
-                canvasCaption: document.querySelector('.canvas-caption')
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas: document.querySelector('.image-blend-canvas'),
+                context: document.querySelector('.image-blend-canvas').getContext('2d'),
+                imagesPath: [
+                    './images/blend-image-1.jpg',
+                    './images/blend-image-2.jpg'
+                ],
+                images: []
             },
             values: {
 
@@ -113,15 +120,22 @@
         let imgElem;
         for(let i=0 ; i<sceneInfo[0].values.videoImageCount; i++) {
             imgElem = new Image();
-            imgElem.src = `/video/001/IMG_${6726 + i}.JPG`;
+            imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
             sceneInfo[0].objs.videoImages.push(imgElem);
         }
 
         let imgElem2;
         for(let i=0 ; i<sceneInfo[2].values.videoImageCount; i++) {
             imgElem2 = new Image();
-            imgElem2.src = `/video/001/IMG_${7027 + i}.JPG`;
+            imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
             sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
+
+        let imgElem3;
+        for(let i=0 ; i<sceneInfo[3].objs.imagesPath.length ; i++) {
+            imgElem3 = new Image();
+            imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+            sceneInfo[3].objs.images.push(imgElem3);
         }
     }
     setCanvasImages();
@@ -296,6 +310,22 @@
 
             case 3:
                 // console.log('3 play');
+                // 가로/세로 모두 꽉차게 하기 위해 여기서 세팅(계산 필요)
+                const widthRatio = window.innerWidth / objs.canvas.width;
+                const heightRatio = window.innerHeight / objs.canvas.height;
+                let canvasScaleRatio;
+
+                if(widthRatio <= heightRatio) {
+                    // 캔버스보다 브라우저 창이 홀쭉한 경우
+                    canvasScaleRatio = heightRatio;
+                } else {
+                    // 캔버스보다 브라우저 창이 납작한 경우
+                    canvasScaleRatio = widthRatio;
+                }
+
+                objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                objs.context.drawImage(objs.images[0], 0, 0);
+
                 break;
         }
     }
