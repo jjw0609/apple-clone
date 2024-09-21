26f00d6d72a5b632ddcac16b9879156c358c6c26
wjdwodnd93 <jjw_0609@naver.com>
2024-09-21 23:06:27 +0900

    이미지 블렌딩

--- js/main.js
+++ js/main.js
@@ -113,6 +113,7 @@
             values: {
                 rect1X: [0, 0, {start: 0, end: 0}],
                 rect2X: [0, 0, {start: 0, end: 0}],
+                blendHeight: [0, 0, {start: 0, end: 0}],
                 rectStartY: 0
             }
         }
@@ -395,8 +396,17 @@
 
                     // 이미지 블렌드
                     // imageBlendY: [0, 0, {start: 0, end: 0}]
-                    objs.context.drawImage(img, x, y, 200, height);
+                    values.blendHeight[0] = 0;
+                    values.blendHeight[1] = objs.canvas.height;
+                    values.blendHeight[2].start = values.rect1X[2].end;
+                    values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
 
+                    const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+                    objs.context.drawImage(objs.images[1],
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+                    );
 
                     objs.canvas.classList.add('sticky');
                     objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
