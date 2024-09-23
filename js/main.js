eeaf0ea93f22e1440f0348114a243ad79c152d6a
wjdwodnd93 <jjw_0609@naver.com>
2024-09-23 22:13:52 +0900

    이미지 블렌딩 후 캔버스 스케일 조절

--- js/main.js
+++ js/main.js
@@ -114,6 +114,7 @@
                 rect1X: [0, 0, {start: 0, end: 0}],
                 rect2X: [0, 0, {start: 0, end: 0}],
                 blendHeight: [0, 0, {start: 0, end: 0}],
+                canvas_scale: [0, 0, {start: 0, end: 0}],
                 rectStartY: 0
             }
         }
@@ -410,6 +411,15 @@
 
                     objs.canvas.classList.add('sticky');
                     objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
+
+                    if(scrollRatio > values.blendHeight[2].end) {
+                        values.canvas_scale[0] = canvasScaleRatio;
+                        values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width);
+                        values.canvas_scale[2].start = values.blendHeight[2].end;
+                        values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
+
+                        objs.canvas.style.trasform = `scale(${calcValues(values.canvas_scale, currentYOffset)});`;
+                    }
                 }
 
                 break;
